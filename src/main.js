05edea1b095af7e13f81866abd1fc72c75c3a4fe
想走过亚洲的熊 <1693889638@qq.com>
2022-04-22 13:35:57 +0800

    主文件启动

---
+++ src/main.js
@@ -0,0 +1,13 @@
+// 项目入口文件,编写最基础的代码(应用程序)
+const Koa = require("koa");
+
+const app = new Koa();
+
+app.use((ctx, next) => {
+  ctx.body = "hello word";
+});
+
+app.listen(3000, () => {
+    
+  console.log("server is running on http://localhost:3000");
+});
